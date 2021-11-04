20339adbe6b335c9fa7fe4e773b8b531e70cf426
Parghuzat <paguzati@hotmail.com>
2021-11-03 18:02:41 -0700

    play

--- play.js
+++ play.js
@@ -1,17 +1,22 @@
-const net = require("net");
+const connect = require('./client')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',// IP adress here,
-    port: 50541 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
-  return conn;
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+  process.exit();
+}
 };
 
 console.log("Connecting ...");
 connect();
+setupInput();
