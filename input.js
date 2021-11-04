f6a9a83f56a6b446a22e33f8bd9fac9e513f55ac
Parghuzat <paguzati@hotmail.com>
2021-11-03 19:11:34 -0700

    snake

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function () {
+let connection;
+
+
+
+const setupInput =  (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,8 +17,17 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
   process.exit();
 }
-  if (key === '\u0020') {
-    console.log('hi');
+  if (key === '\u0077') {  //w
+    connection.write("Move: up");
+  }
+  if (key === '\u0061') { //a
+  connection.write("Move: left");
+}
+  if (key === '\u0073') { //s
+    connection.write("Move: down");
+  }
+  if (key === '\u0064') { //d
+    connection.write("Move: right");
   }
 };
 
